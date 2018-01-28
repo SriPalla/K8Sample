49b87182dab7e72a1b8d63bdd9cc1fc8f071750e
Srikanth Palla <sri3012kanth@outlook.com>
2018-01-28 17:30:53 +0530

    added get call

--- Angular-Sample/src/app/app.component.ts
+++ Angular-Sample/src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  records: any[];
+
+  constructor(private http: HttpClient){
+  }
+
+  ngOnInit(): void {
+    this.http.get('http://counterweb/getAllBics').subscribe(data => {
+      console.log(data.response);
+    });
+  }
+
 }
