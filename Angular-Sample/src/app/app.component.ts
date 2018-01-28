f2dfb66ada8492d7e8bc535a4f4495c0146b8d46
Srikanth Palla <sri3012kanth@outlook.com>
2018-01-28 19:13:16 +0530

    applied styling

--- Angular-Sample/src/app/app.component.ts
+++ Angular-Sample/src/app/app.component.ts
@@ -8,7 +8,7 @@ import { HttpClient, HttpHeaders, HttpClientModule } from '@angular/common/http'
   providers: [HttpClient]
 })
 export class AppComponent implements OnInit {
-  records: any[];
+  records: any;
 
   constructor(private http: HttpClient) {
   }
@@ -22,7 +22,15 @@ export class AppComponent implements OnInit {
 
     this.http.get('http://counterweb:8080/getAllBics', httpOptions).subscribe(data => {
       console.log(data);
+      this.records = data;
     });
   }
 
+}
+export interface BicData {
+  response: Bic[]
+}
+export interface Bic {
+  bicCode: string;
+  description: string;
 }
